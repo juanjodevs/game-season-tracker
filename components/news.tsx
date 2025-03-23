6538527d4da448ff2c358ff53cf01b2137c0ef6a
Juanjo Martínez <juanjodevs@gmail.com>
2025-03-23 08:01:27 +0100

    Format date on news

--- components/news.tsx
+++ components/news.tsx
@@ -1,15 +1,28 @@
+import { formatDate } from '@/lib/utils'
 import { Card, Heading } from '@radix-ui/themes'
-const News = ({ news }: { news: string[] }) => (
-  <section className='pb-5'>
-    <Card className='bg-yellow-300 border-2 border-yellow-700 text-slate-800'>
-      <Heading>Latest news</Heading>
-      <ul>
-        {
-          news.map((item, i) => (<li key={i} className='ml-5 list-disc' >{item}</li>))
-        }
-      </ul>
-    </Card>
-  </section>
-)
+
+const News = ({ news }: { news: string[] }) => {
+  const formatNewDate = (item: string) => {
+    const reg = /[0-9]{4}-[0-9]{2}-[0-9]{2}/g
+    const match = item.match(reg)
+    if (match) {
+      return item.replace(reg, formatDate(match[0]))
+    }
+    return item
+  }
+
+  return (
+    <section className='pb-5'>
+      <Card className='bg-yellow-300 border-2 border-yellow-700 text-slate-800'>
+        <Heading>Latest news</Heading>
+        <ul>
+          {
+            news.map((item, i) => (<li key={i} className='ml-5 list-disc' >{formatNewDate(item)}</li>))
+          }
+        </ul>
+      </Card>
+    </section>
+  )
+}
 
 export default News
